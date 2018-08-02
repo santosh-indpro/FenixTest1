d8cb25bd662f4a3ab7bd7b3c049d21abefa78c35
TTX <prakharsantosh@gmail.com>
2018-08-02 20:09:01 +0530

    5

--- src/components/common/preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
-
-const PreLoader = ({ spokes, red }) => (
-  <ReactLoading type="spokes" color="green" height={100} width={100} />
-);
-
-export default PreLoader;
